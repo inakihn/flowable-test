e86a4d6047bf420293e3a11a7afee934b888fdef
inaki.huete <inaki.huete@flowable.com>
2018-09-05 16:34:14 +0200

    Update to 6.3.1

--- src/test/java/com/inaki/MyUnitTest.java
+++ src/test/java/com/inaki/MyUnitTest.java
@@ -1,12 +1,14 @@
 package com.inaki;
+
 import org.flowable.engine.runtime.ProcessInstance;
-import org.flowable.engine.task.Task;
-import org.flowable.engine.test.FlowableRule;
 import org.flowable.engine.test.Deployment;
+import org.flowable.engine.test.FlowableRule;
+import org.flowable.task.api.Task;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class MyUnitTest {
 
